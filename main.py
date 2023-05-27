083fdc0bd34ec8e004ad3195a7f76151255e7095
RaadJarjees <66251677+RaadJarjees@users.noreply.github.com>
2023-05-28 00:51:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-#!/bin/env python3
 
-import os, sys
-sys.path.append(os.getcwd()+"/.lib/")
-import argparse
-from api import *
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-u", "--user", required=True, help="username of account to scan")
-ap.add_argument("-p", "--post", action="store_true", help="image info of user uploads")
-args = vars(ap.parse_args())
+def user_info(usrname):
+    url = f"https://example.com/api/user/{usrname}"  # Replace 'example.com' with the actual API endpoint
+    resp = requests.get(url)
+
+    if resp.status_code == 200:
+        resp_js = resp.text
+        try:
+            js = json.loads(resp_js)
+            # Process the JSON data here
+            print(js)  # Example: Print the JSON data
+        except json.JSONDecodeError as e:
+            print("Error decoding JSON:", e)
+    else:
+        print("Error:", resp.status_code)
+
+
+
+
+
+
+
+
+
 	
-os.system("clear")
 
-if args['user']:
-	user_info(usrname=args["user"])
 
-if args['post']:
-	post_info()
+
+
+
+
+	
